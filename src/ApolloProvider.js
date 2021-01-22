0b73e5d774e30edd6107bc8c424e9bfdb2752c76
Vincenzo Mezzio <vgmezzio@gmail.com>
2021-01-22 01:50:11 -0500

    client

---
+++ src/ApolloProvider.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import App from './App'
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { setContext } from 'apollo-link-context'
+// Apparently i am only supposed to use apollo/client. apollo react hook sis outdated but just using client isnt working
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:5000'
+})
+
+const authLink = setContext(() => {
+    const token = localStorage.getItem('jwtToken');
+    return{
+        headers: {
+            Authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+})
+
+export default (
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+)
